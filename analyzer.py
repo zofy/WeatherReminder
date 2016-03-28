e52f7b81bd600ad5a15d85af89312ba12c907139
zofy <zofy11@gmail.com>
2016-03-28 18:13:07 +0200

    sender - drobne zmeny analyzer - 2 objekty : Locator - najde polohu a miesto Analyzer - vyhodnoti pocasie

---
+++ analyzer.py
@@ -0,0 +1,52 @@
+import urllib2
+import json
+
+import requests
+from bs4 import BeautifulSoup
+
+
+class Locator(object):
+    loc_url = "https://geoiptool.com"
+
+    def __init__(self, loc_data={}):
+        self.loc_data = loc_data
+
+    def __str__(self):
+        return self.loc_data
+
+    def get_location(self):
+        r = requests.get(self.loc_url)
+        soup = BeautifulSoup(r.content, "lxml")
+        items = soup.find_all('div', {"class": "data-item"})
+        for item in items:
+            spans = item.find_all('span')
+            category = str(spans[0].text)[:-1]
+            if category in ('Latitude', 'Longitude', 'City'):
+                self.loc_data[category] = str(spans[1].text.strip())
+
+
+class Analyzer(object):
+    base = 'http://my.meteoblue.com/feed/json_7day_3h_firstday?apikey=sk489ywfh498hjf'
+
+    def __init__(self, lat, long, city):
+        self.lat = lat
+        self.long = long
+        self.city = city
+
+    def __str__(self):
+        return 'Getting data for {lat: %s, long: %s, city: %s}' % (self.lat, self.long, self.city)
+
+    def build_url(self):
+        return self.base + '&lat=' + self.lat + '&long=' + self.long + '&city=' + self.city
+
+    def get_data(self):
+        meteo_url = self.build_url()
+        json_data = urllib2.urlopen(meteo_url)
+        data = json.load(json_data)
+        return data
+
+
+l = Locator()
+print(l.loc_data)
+l.get_location()
+print(l.loc_data)
--- sender.py
+++ sender.py
@@ -1,63 +1,21 @@
-# # Import smtplib for the actual sending function
-# import smtplib
-#
-# # Import the email modules we'll need
-# from email.mime.text import MIMEText
-#
-# # Open a plain text file for reading.  For this example, assume that
-# # the text file contains only ASCII characters.
-# # fp = open(textfile, 'rb')
-# # Create a text/plain message
-# # msg = MIMEText(fp.read())
-# msg = MIMEText('Hello can you hear me?')
-# # fp.close()
-#
-# # me == the sender's email address
-# # you == the recipient's email address
-# msg['Subject'] = 'The contents of mail'
-# msg['From'] = 'me'
-# msg['To'] = 'zofy11@gmail.com'
-#
-# # Send the message via our own SMTP server, but don't include the
-# # envelope header.
-# s = smtplib.SMTP('localhost')
-# s.sendmail('me', 'zofy11@gmail.com', msg.as_string())
-# s.quit()
-
-
-# import smtplib
-#
-# sender = 'zofy11@gmail.com'
-# receivers = ['zofy11@gmail.com']
-#
-# message = """From: From Person <from@fromdomain.com>
-# To: To Person <to@todomain.com>
-# Subject: SMTP e-mail test
-#
-# This is a test e-mail message.
-# """
-#
-# try:
-#    smtpObj = smtplib.SMTP('localhost')
-#    smtpObj.sendmail(sender, receivers, message)
-#    print "Successfully sent email"
-# except smtplib.SMTPException:
-#    print "Error: unable to send email"
-
-
 import smtplib
-import email.utils
+from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-# Create the message
-msg = MIMEText('This is the body of the message.')
-msg['To'] = email.utils.formataddr(('Recipient', 'zofy11@gmail.com'))
-msg['From'] = email.utils.formataddr(('Author', 'zofy11@gmail.com'))
-msg['Subject'] = 'Simple test message'
-
-server = smtplib.SMTP('127.0.0.1', 1025)
-server.set_debuglevel(True) # show communication with the server
-try:
-    server.sendmail('zofy11@gmail.com', ['zofy11@gmail.com'], msg.as_string())
-finally:
-    server.quit()
+from_address = 'zofy11@gmail.com'
+to_address = 'zofy11@gmail.com'
+text = 'test message sent from Python'
+username = 'xxxxx'
+password = 'xxxxx'
+msg = MIMEMultipart()
+msg['From'] = from_address
+msg['To'] = to_address
+msg['Subject'] = 'Weather forecast'
+msg.attach(MIMEText(text))
+server = smtplib.SMTP('smtp.gmail.com:587')
+server.ehlo()
+server.starttls()
+server.ehlo()
+server.login(username, password)
+server.sendmail(from_address, to_address, msg.as_string())
+server.quit()
